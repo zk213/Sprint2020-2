54e8d56ba2d5c40cb279ff1501ce2b6bd978e4af
Kai Zhuang <35880996+zk213@users.noreply.github.com>
2020-03-02 17:15:55 -0500

    Rotate movement

---
+++ Sprint2020-2_Project/Assets/Scripts/Controller.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public float leftWalkSpeed;
+    public float rightWalkSpeed;
+
+
+
+    public float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            angle -= leftWalkSpeed;
+            angle = angle % 360;
+
+            transform.eulerAngles = new Vector3(0, 0, angle);
+
+
+        }
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+
+        {
+
+            angle += rightWalkSpeed;
+            angle = angle % 360;
+
+            transform.eulerAngles = new Vector3(0, 0, angle);
+
+
+
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+
+        }
+
+
+    }
+
+
+
+}
