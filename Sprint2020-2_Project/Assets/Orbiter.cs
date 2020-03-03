3d84a90f81dff82388378d733785c8aeea6091b5
AlessandroPro <alessandro.profenna@gmail.com>
2020-03-02 20:58:34 -0500

    Created basic prototype, with orbitting players and bullets that can be shot

---
+++ Sprint2020-2_Project/Assets/Orbiter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiter : MonoBehaviour
+{
+    private const float MIN_RADIUS = 1;
+    private const float MAX_RADIUS = 5;
+
+    [Range(MIN_RADIUS, MAX_RADIUS)]
+    public float radius = 1;
+    public float speed = 0;
+    public bool moveClockwise = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = transform.position.magnitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int moveDir = 1;
+        if(moveClockwise)
+        {
+            moveDir = -1;
+        }
+
+        transform.position = transform.position.normalized * radius;
+        transform.RotateAround(Vector3.zero, Vector3.forward, speed * moveDir * Time.deltaTime);
+    }
+}
---
+++ Sprint2020-2_Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float pullSpeed = 1;
+    public bool isFirstPlayer = true;
+    public GameObject bulletPrefab;
+    private Orbiter orbiter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        orbiter = GetComponent<Orbiter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if(isFirstPlayer)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                orbiter.radius += pullSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                orbiter.radius -= pullSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                shoot();
+            }
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                orbiter.moveClockwise = !orbiter.moveClockwise;
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                orbiter.radius += pullSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                orbiter.radius -= pullSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                shoot();
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                orbiter.moveClockwise = !orbiter.moveClockwise;
+            }
+        }
+    }
+
+    void shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab);
+        bullet.transform.position = transform.position;
+
+        var angle = Mathf.Atan2(transform.position.y, transform.position.x) * Mathf.Rad2Deg;
+        bullet.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        Orbiter bulletOrbiter = bullet.GetComponent<Orbiter>();
+        bulletOrbiter.speed = orbiter.speed * 2;
+        bulletOrbiter.moveClockwise = orbiter.moveClockwise;
+    }
+}
