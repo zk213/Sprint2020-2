2774e6c4a71de9ac2223154bccdebd7ed02f50d3
Ethan Davis <daviseth@sheridancollege.ca>
2020-03-03 15:31:22 -0500

    Bullet death timer

--- Sprint2020-2_Project/Assets/Bullet.cs
+++ Sprint2020-2_Project/Assets/Bullet.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 5f);
     }
 
     // Update is called once per frame
@@ -20,5 +21,9 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(this.gameObject);
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
