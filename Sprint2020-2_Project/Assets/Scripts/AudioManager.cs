4fabfd7b5a2160a47c9f3e7775b81976d7fa8a55
Kai Zhuang <35880996+zk213@users.noreply.github.com>
2020-03-04 16:02:45 -0500

    StartMenu,LevelChanger,TutorialPannel

---
+++ Sprint2020-2_Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    //I learned this script from a Youtuber called Brackeys
+    //And i followed his video and typed the code
+
+
+    //Set a list to store sounds
+    public Sound[] sounds;
+
+
+    void Start()
+    {
+        //The sound that i need to play from the beginning of the game which is the BGM
+    }
+
+    void Awake()
+    {
+        //Set audio properties through code
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.vloume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    //The code for looking for sound when i need them
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
---
+++ Sprint2020-2_Project/Assets/Scripts/UIlogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIlogic : MonoBehaviour
+{
+    Animator m_animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ShowTutorialPannel()
+    {
+        m_animator.SetTrigger("Move");
+
+    }
+
+    
+}
