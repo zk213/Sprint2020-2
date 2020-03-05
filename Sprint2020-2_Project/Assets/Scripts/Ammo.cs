5cdf5195afdb676c7ae58b1277d22f367c2569c3
Ethan Davis <daviseth@sheridancollege.ca>
2020-03-04 19:59:48 -0500

    Implemented ammo bar to old version

--- Sprint2020-2_Project/Assets/Scripts/Ammo.cs
+++ Sprint2020-2_Project/Assets/Scripts/Ammo.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ammo : MonoBehaviour
 {
     public int currentAmmo;
     public int maxAmmo = 3;
+    public Image[] bullets;
     float regenerateSpeed = 1.5f;
 
 
@@ -19,7 +21,36 @@ public class Ammo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentAmmo == 3)
+        {
+            bullets[0].fillAmount = 1;
+            bullets[1].fillAmount = 1;
+            bullets[2].fillAmount = 1;
+        }
+
+        if (currentAmmo == 2)
+        {
+            bullets[0].fillAmount = 0;
+            bullets[1].fillAmount = 1;
+            bullets[2].fillAmount = 1;
+        }
+
+        if (currentAmmo == 1)
+        {
+            bullets[0].fillAmount = 0;
+            bullets[1].fillAmount = 0;
+            bullets[2].fillAmount = 1;
+        }
+
+        if (currentAmmo == 0)
+        {
+            bullets[0].fillAmount = 0;
+            bullets[1].fillAmount = 0;
+            bullets[2].fillAmount = 0;
+        }
+
+
+
     }
 
     public void consumeAmmo()
